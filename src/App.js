606f4071a93e03692d099c620ddb7cdbcb2cefee
Rory Doak <rory.doak@gmail.com>
2023-11-18 15:34:42 +0000

    initial scatter plot component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import "./App.css";
+import "./App.scss";
+import ScatterPlot from "./components/ScatterPlot/ScatterPlot";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ScatterPlot />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/ScatterPlot/ScatterPlot.js
@@ -0,0 +1,72 @@
+import { ResponsiveScatterPlot } from "@nivo/scatterplot";
+import "./ScatterPlot.scss";
+import { useEffect, useState } from "react";
+
+export default function ScatterPlot() {
+  const [data, setData] = useState(null);
+  const userId = "57581dd2-96b8-4402-912b-c669c16f21a2";
+  useEffect(() => {
+    const fetchBalanceData = async () => {
+      const balances = await axios.get(
+        `http://localhost:8080/accounts/connections/${userId}`
+      );
+      setData(await balances);
+    };
+  }, userId);
+  return (
+    <section className="scatter-plot">
+      <ResponsiveScatterPlot
+        data={data}
+        margin={{ top: 60, right: 140, bottom: 70, left: 90 }}
+        xScale={{ type: "linear", min: 0, max: "auto" }}
+        xFormat=">-.2f"
+        yScale={{ type: "linear", min: 0, max: "auto" }}
+        yFormat=">-.2f"
+        blendMode="multiply"
+        axisTop={null}
+        axisRight={null}
+        axisBottom={{
+          orient: "bottom",
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: "weight",
+          legendPosition: "middle",
+          legendOffset: 46,
+        }}
+        axisLeft={{
+          orient: "left",
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: "size",
+          legendPosition: "middle",
+          legendOffset: -60,
+        }}
+        legends={[
+          {
+            anchor: "bottom-right",
+            direction: "column",
+            justify: false,
+            translateX: 130,
+            translateY: 0,
+            itemWidth: 100,
+            itemHeight: 12,
+            itemsSpacing: 5,
+            itemDirection: "left-to-right",
+            symbolSize: 12,
+            symbolShape: "circle",
+            effects: [
+              {
+                on: "hover",
+                style: {
+                  itemOpacity: 1,
+                },
+              },
+            ],
+          },
+        ]}
+      />
+    </section>
+  );
+}
