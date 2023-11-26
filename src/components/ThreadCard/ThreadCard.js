df389861e68f35e5f4a4cc8d24749f137562cbb8
Rory Doak <rory.doak@gmail.com>
2023-11-26 20:23:09 +0000

    add thread list for homepage

--- src/components/ThreadCard/ThreadCard.js
+++ src/components/ThreadCard/ThreadCard.js
@@ -1,11 +1,51 @@
+import { useParams } from "react-router";
 import Thread from "../Thread/Thread";
 import "./ThreadCard.scss";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { LinearProgress } from "@mui/material";
 
 export default function ThreadCard() {
+  const [threadArray, setThreadArray] = useState(null);
+  const { userId } = useParams();
+
+  const getThreads = async () => {
+    try {
+      const { data } = await axios.get(
+        `http://localhost:8080/threads/${userId}`
+      );
+      setThreadArray(data);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  console.log(threadArray);
+
+  useEffect(() => {
+    getThreads();
+  }, [userId]);
+
+  if (!threadArray) {
+    return <LinearProgress />;
+  }
+
   return (
     <section className="thread-card">
       <h2 className="thread-card__title">Threads</h2>
-      <Thread />
+      {threadArray.map((thread, index) => {
+        if (index > 2) {
+          return "";
+        }
+        return (
+          <Thread
+            key={thread.id}
+            name={`${thread.thread_name}`}
+            userId={userId}
+            threadId={thread.thread_id}
+          />
+        );
+      })}{" "}
     </section>
   );
 }
